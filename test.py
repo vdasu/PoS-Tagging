621218a16c638b6dbbef3c66ee2ad6c4abd24702
Vishnu Dasu <vishnu98dasu@gmail.com>
2017-06-11 19:35:27 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,11 @@
-from nltk.corpus import brown
+from time import time
 from tokenizer import word_tokenize
     
-for i,j in zip(brown.sents(),brown.tagged_sents()):
-    tokens = word_tokenize(' '.join(i))
-    for x,y in zip(i,(str(z[0]) for z in j)):
-        if x!=y:
-            print ("MISMATCH")
-            exit()
-        else:
-            print (x," ",y)
+text = open("brown.txt","r").read()
+t1 = time()
+tokens = word_tokenize(text)
+t2 = time()
+print ("Time taken to tokenize: ",t2-t1)
+input("Press Enter to view tokens")
+print (tokens)
         
