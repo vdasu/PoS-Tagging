50d3ce6afa8c8182586af079f05a53dc1313b3c0
Vishnu Dasu <vishnu98dasu@gmail.com>
2017-06-20 22:46:39 +0530

    Update regex

--- tokenizer.py
+++ tokenizer.py
@@ -5,5 +5,5 @@ def word_tokenize(text):
     tokens = [];
     sents = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<![A-Z]\.)(?<=\.|\?)\s',text) #Regex to tokenize sentences
     for txt in sents:
-        tokens.append(re.findall(r"\w+(?:\.|-\w+)+|[\w']+|[^\w\s]",txt)) #Regex to tokenize words
+        tokens.append(re.findall(r"[-]{2}|\w+(?:\.(?=\S)|-\w+)+|[\w']+|[^\w\s]",txt)) #Regex to tokenize words
     return tokens #List of tokens of each sentence in raw text returned
