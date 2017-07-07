cab50ed4479a8129bb788dfa4ae29067de058453
Vishnu Dasu <vishnu98dasu@gmail.com>
2017-07-07 20:07:16 +0530

    Add own probability and frequency distributions

--- pos_tagger.py
+++ pos_tagger.py
@@ -1,104 +1,97 @@
-from nltk.probability import ConditionalFreqDist as CFD
-from nltk.probability import ConditionalProbDist as CPD
-from nltk.probability import MLEProbDist
 from nltk.util import ngrams
 from tokenizer import word_tokenize
+from collections import defaultdict
 import re
 
 class pos_tagger:
+    
     def __init__(self):
         self.sents = [] #List containing tokenized sentences
         self.tags = [] #List containing all tags used in Brown Corpus
         self.tags_set = [] #List containing set of all tags used in Brown Corpus
-        self.tags_tokens = [] #List containing token and respective tag as a tuple
-        self.cfddist_tagswords = None #Conditional Frequency Distribution of words over tags
-        self.cpddist_tagswords = None #Conditional Probability Distribution of words over tags
-        self.cfddist_tags = None #Conditional Frequency Distribution of tags as trigrams
-        self.cpddist_tags = None #Conditional Probability Distribution of tags as trigrams
+        self.words_tags = [] #List containing token and respective tag as a tuple
+        self.tagged_sents = [] #List containing tokenized tagged sentences
         self.trigrams_as_bigrams = [] #List containing trigrams of tokens as bigrams 
-    def tokenize(self):
+        self.words_tags_dict = defaultdict(int) #Dictionary to store count of word-tag pairs
+        self.trigrams_dict = defaultdict(int) #Dictionary to store count of tags as trigrams
+        self.bigrams_dict = defaultdict(int) #Dictionary to store count of tags as bigrams
+        self.unigrams_dict = defaultdict(int) #Dictionary to store count of tags as unigrams
+        self.tags_prob = defaultdict(float) #Dictionary to store conditional probability of tag trigrams
+        self.words_tags_prob = defaultdict(float) #Dictionary to store conditional probability of word-tag pairs
+    
+    def tokenize(self): #Function to tokenize text
         text = open("brown.txt","r").read()
         self.sents = word_tokenize(text)
         for sent in self.sents:
             sent[:0] = ['START','START'] #Append START token to beginning of list
             sent.append('STOP') #Append STOP token to end of list
-    def init_tags(self):
+    
+    def init_tags(self): #Function to initialise tags
         sents = open("brown_tagged.txt").read().splitlines()
         for sent in sents:
             words = sent.split()
             for word in words:
                 m = re.search('_(.*)',word)
                 self.tags.append(m.group(1))
-        self.tags_set = set(self.tags)
-    def init_tokens_tags(self):
+        self.tags_set = set(self.tags) #List of all unique tags in corpus
+    
+    def init_words_tags(self): #Function to initialise word-tag pairs
         tagged_sents = open("brown_tagged.txt").read().splitlines()
-        tags_tokens = []
+        words_tags = []
         for tagged_sent in tagged_sents:
             tagged_words = tagged_sent.split()
             for tagged_word in tagged_words:
                 m = re.search('(.*)_(.*)',tagged_word)
-                tags_tokens.append((m.group(2),m.group(1)))
-            self.tags_tokens.append(tags_tokens)
-            tags_tokens = []
-    def init_cfd_tagswords(self):
-        self.cfddist_tagswords = CFD(j for i in self.tags_tokens for j in i)
-    def init_cpd_tagswords(self):
-        self.cpddist_tagswords = CPD(self.cfddist_tagswords,MLEProbDist)
-    def hmm_trigram(self):
-        trigrams = ngrams(self.tags,3)
-        self.trigrams_as_bigrams.extend([((tags[0],tags[1]),tags[2]) for tags in trigrams])
-        self.cfddist_tags = CFD(self.trigrams_as_bigrams)
-        self.cpddist_tags = CPD(self.cfddist_tags,MLEProbDist)
-    def hmm_test(self):
-        sent = "START START The grand jury commented. STOP"
-        tag_sequence = "START START AT JJ NN VBD . STOP"
-        prob_tags = 1.0
-        prob_tagswords = 1.0
-        words_tokens = [j for i in word_tokenize(sent) for j in i]
-        tags_tokens = [j for i in word_tokenize(tag_sequence) for j in i]
-        for i in range(2,len(tags_tokens)):
-            prob_tags*=self.cpddist_tags[(tags_tokens[i-2],tags_tokens[i-1])].prob(tags_tokens[i])
-        for i,j in zip(words_tokens,tags_tokens):
-            prob_tagswords*=(self.cpddist_tagswords[j].prob(i))
-        prob_tot = prob_tags*prob_tagswords
-        print ('The probability of "START START The grand jury commented. STOP" having tags "START START AT JJ NN VBD . STOP" is: ',prob_tot)
-    def bigram_hmm_test(self):
-        sent = "START The grand jury commented. STOP"
-        tag_sequence = "START AT JJ NN VBD . STOP"
-        tags_tokens = self.tags_tokens
-        for i in tags_tokens:
-            i.remove(('START','START'))
-        sents = open('brown_tagged.txt').read().splitlines()
-        tags = []
-        for sent in sents:
-            words = sent.split()
-            words.remove('START_START')
-            for word in words:
-                m = re.search('_(.*)',word)
-                tags.append(m.group(1)) 
-        cfddist_tagswords = CFD(j for i in tags_tokens for j in i)
-        bigrams = ngrams(tags,2)
-        cfddist_tags = CFD(bigrams)
-        cpddist_tagswords = CPD(cfddist_tagswords,MLEProbDist)
-        cpddist_tags = CPD(cfddist_tags,MLEProbDist)
-        prob_tags = 1.0
-        prob_tagswords = 1.0
-        words_tokens = [j for i in word_tokenize(sent) for j in i]
-        tags_tokens = [j for i in word_tokenize(tag_sequence) for j in i]
-        for i in range(1,len(tags_tokens)):
-            prob_tags*=cpddist_tags[tags_tokens[i-1]].prob(tags_tokens[i])
-        for i,j in zip(words_tokens,tags_tokens):
-            prob_tagswords*=cpddist_tagswords[j].prob(i)
-        prob_tot=prob_tags*prob_tagswords
-        print ("Bigram probability: ",prob_tot)
+                words_tags.append((m.group(1),m.group(2)))
+            self.words_tags.append(words_tags)
+            words_tags = []
+        self.tagged_sents = self.words_tags
+        self.words_tags = [j for i in self.words_tags for j in i]
+    
+    def init_dict(self): #Function to initialised dictionaries
+    	for i in self.words_tags:
+    		self.words_tags_dict[i]+=1   
+    	trigrams_tags = ngrams(self.tags,3)
+    	for i in trigrams_tags:
+    		self.trigrams_dict[i]+=1
+    	bigrams_tags = ngrams(self.tags,2)
+    	for i in bigrams_tags:
+    		self.bigrams_dict[i]+=1
+    	unigrams_tags = ngrams(self.tags,1)
+    	for i in unigrams_tags:
+    		self.unigrams_dict[i]+=1
+    
+    def calc_Q(self): #Function to calculate tag probabilities
+    	trigrams_tags = ngrams(self.tags,3)
+    	for i,j,k in trigrams_tags:
+    		self.tags_prob[(i,j,k)]=float(self.trigrams_dict[(i,j,k)])/float(self.bigrams_dict[(i,j)])
+    
+    def calc_R(self): #Function to calculate word-tag probabilities
+    	for word,tag in self.words_tags:
+    		self.words_tags_prob[(word,tag)]=float(self.words_tags_dict[(word,tag)])/float(self.unigrams_dict[(tag,)])
 
+    def test(self): #Test function
+
+    	sent = "START START The grand jury commented. STOP"
+    	tag = "START START AT JJ NN VBD . STOP"
+    	prob_tag = 1.0
+    	prob_words = 1.0
+    	tot_prob = 1.0
+    	sent_tokens = [j for i in word_tokenize(sent) for j in i]
+    	tag_tokens = [j for i in word_tokenize(tag) for j in i]
+    	for i in range(2,len(tag_tokens)-1):
+    		prob_tag*=self.tags_prob[(tag_tokens[i-2],tag_tokens[i-1],tag_tokens[i])]
+    	for i,j in zip(sent_tokens,tag_tokens):
+    		prob_words*=self.words_tags_prob[(i,j)]
+    	tot_prob=prob_tag*prob_words
+    	print (tot_prob)
+   
 if __name__=="__main__":
     pos_tag = pos_tagger()
     pos_tag.tokenize()
     pos_tag.init_tags()
-    pos_tag.init_tokens_tags()
-    pos_tag.init_cfd_tagswords()
-    pos_tag.init_cpd_tagswords()
-    pos_tag.hmm_trigram()
-    pos_tag.hmm_test()
-    pos_tag.bigram_hmm_test()
+    pos_tag.init_words_tags()
+    pos_tag.init_dict()
+    pos_tag.calc_Q()
+    pos_tag.calc_R()
+    pos_tag.test()
