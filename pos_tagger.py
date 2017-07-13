42c363b15dd3c35c77bafac8e6f345929a396254
Vishnu Dasu <vishnu98dasu@gmail.com>
2017-07-13 13:44:54 +0530

    Update variables and add baseline_tagger

--- pos_tagger.py
+++ pos_tagger.py
@@ -1,6 +1,8 @@
 from nltk.util import ngrams
 from tokenizer import word_tokenize
 from collections import defaultdict
+from time import time
+from os import stat
 import re
 
 class pos_tagger:
@@ -16,8 +18,8 @@ class pos_tagger:
         self.trigrams_dict = defaultdict(int) #Dictionary to store count of tags as trigrams
         self.bigrams_dict = defaultdict(int) #Dictionary to store count of tags as bigrams
         self.unigrams_dict = defaultdict(int) #Dictionary to store count of tags as unigrams
-        self.tags_prob = defaultdict(float) #Dictionary to store conditional probability of tag trigrams
-        self.words_tags_prob = defaultdict(float) #Dictionary to store conditional probability of word-tag pairs
+        self.Q = defaultdict(float) #Dictionary to store conditional probability of tag trigrams
+        self.R = defaultdict(float) #Dictionary to store conditional probability of word-tag pairs
     
     def tokenize(self): #Function to tokenize text
         text = open("brown.txt","r").read()
@@ -32,10 +34,6 @@ class pos_tagger:
             words = sent.split()
             for word in words:
                 m = re.search('_(.*)',word)
-                # if m.group(1) in ('START','STOP'):
-                #     self.tags.append(m.group(1))
-                # else:
-                #     self.tags.append(m.group(1)[:2])
                 self.tags.append(m.group(1))
         self.tags_set = set(self.tags) #List of all unique tags in corpus
     
@@ -46,10 +44,6 @@ class pos_tagger:
             tagged_words = tagged_sent.split()
             for tagged_word in tagged_words:
                 m = re.search('(.*)_(.*)',tagged_word)
-                # if m.group(2) in ('START','STOP'):
-                #     words_tags.append((m.group(1),m.group(2)))
-                # else:
-                #     words_tags.append((m.group(1),m.group(2)[:2]))
                 words_tags.append((m.group(1),m.group(2)))
             self.words_tags.append(words_tags)
             words_tags = []
@@ -72,11 +66,11 @@ class pos_tagger:
     def calc_Q(self): #Function to calculate tag probabilities
         trigrams_tags = ngrams(self.tags,3)
         for i,j,k in trigrams_tags:
-            self.tags_prob[(i,j,k)]=float(self.trigrams_dict[(i,j,k)])/float(self.bigrams_dict[(i,j)])
+            self.Q[(i,j,k)]=float(self.trigrams_dict[(i,j,k)])/float(self.bigrams_dict[(i,j)])
     
     def calc_R(self): #Function to calculate word-tag probabilities
         for word,tag in self.words_tags:
-            self.words_tags_prob[(word,tag)]=float(self.words_tags_dict[(word,tag)])/float(self.unigrams_dict[(tag,)])
+            self.R[(word,tag)]=float(self.words_tags_dict[(word,tag)])/float(self.unigrams_dict[(tag,)])
 
     def test(self): #Test function
 
@@ -88,22 +82,20 @@ class pos_tagger:
         sent_tokens = [j for i in word_tokenize(sent) for j in i]
         tag_tokens = [j for i in word_tokenize(tag) for j in i]
         for i in range(2,len(tag_tokens)-1):
-            prob_tag*=self.tags_prob[(tag_tokens[i-2],tag_tokens[i-1],tag_tokens[i])]
+            prob_tag*=self.Q[(tag_tokens[i-2],tag_tokens[i-1],tag_tokens[i])]
         for i,j in zip(sent_tokens,tag_tokens):
-            prob_words*=self.words_tags_prob[(i,j)]
+            prob_words*=self.R[(i,j)]
         tot_prob=prob_tag*prob_words
         print (tot_prob)
         
-        print (self.viterbi())
 
-    def viterbi(self):
+    def viterbi(self,tokens): #Function returns list containing tokens and their respective POS tags
         
-        sent = 'However, the jury said it believes "these two offices should be combined to achieve greater efficiency and reduce the cost of administration".'
-        tokens = [j for i in word_tokenize(sent) for j in i] #Tokens of input sentence
         n = len(tokens) #Length of input sentence
         psi = [defaultdict(float) for i in range(n+1)] #List of dictionaries to store max probability of preceding tag sequence at a given postion
-        back_ptr = [defaultdict(str) for i in range(n+1)] #List of dictionaries to store backpointers (tags) that maximises phi
+        back_ptr = [defaultdict(str) for i in range(n+1)] #List of dictionaries to store backpointers (tags) that maximises psi
         psi[0][('START','START')] = 1.0 #Base case
+
         #Iterate through all tokens all store max probabilities and max arguments
         for k in range(1,n+1):
             x = tokens[k-1]
@@ -116,37 +108,71 @@ class pos_tagger:
                 U = ('START')
             if k==2: #One of the preceding tags for second word is START in a trigram model
                 W = ('START')
-            for u in U:
-                for v in V:
+            for u in U: #Tag at position k-1
+                for v in V: #Tag at position k
                     max_prob = 0.0 #Stores maximum probability for each word
                     max_arg = "" #Stores tag that maximises probability 
-                    for w in W:
-                        temp_prob = psi[k-1][(w,u)]*self.tags_prob[(w,u,v)]*self.words_tags_prob[(x,v)] #Calculate probability for tag sequence at position k
+                    for w in W: #Tag at position k-2
+                        temp_prob = psi[k-1][(w,u)]*self.Q[(w,u,v)]*self.R[(x,v)] #Calculate probability for tag sequence at position k
                         if temp_prob>max_prob:
                             if temp_prob!=0.0:
                                 max_prob = temp_prob
                                 max_arg = w 
                     psi[k][(u,v)] = max_prob #Store maximum probability for positon k and tags u,v
-                    back_ptr[k][(u,v)] = max_arg #Store tag that maximises probability (backpointer)
+                    back_ptr[k][(u,v)] = max_arg #Store tag that maximises psi (backpointer)
 
             #Back Track to determine tag sequence
-            max_prob = 0.0
-            max_u = ""
+            max_prob = 0.0 
+            max_u = "" 
             max_v = ""
-            for u in self.tags_set:
-                for v in self.tags_set:
-                    temp_prob = psi[n][(u,v)]*self.tags_prob[(u,v,'STOP')]
+            for u in self.tags_set: #Tag at position n-1
+                for v in self.tags_set: #Tag at position n
+                    temp_prob = psi[n][(u,v)]*self.Q[(u,v,'STOP')]
                     if temp_prob>max_prob:
                         if temp_prob!=0.0:
                             max_prob = temp_prob
                             max_u = u
                             max_v = v
+
             t = [None]*(n+1) #Initialise tag sequence
             t[n] = max_v
             t[n-1] = max_u
             for k in range(n-2,0,-1):
-                t[k] = back_ptr[k+2][(t[k+1],t[k+2])]
-            return t
+                t[k] = back_ptr[k+2][(t[k+1],t[k+2])] #Back track to complete tag sequence
+            
+            tagged_sent = [] #List to store token-tag tuple 
+            for token,tag in zip(tokens,t[1:]):
+                tagged_sent.append((token,tag))
+            return tagged_sent
+
+
+    def baseline_tagger(self):
+
+        from nltk.corpus import brown
+        from nltk.tag import TrigramTagger 
+
+        f = open("input.txt","r").read()
+        
+        file_info = stat("input.txt")
+
+        print ("Size of test file: ",file_info.st_size)
+
+        t0 = time()
+        tagger = TrigramTagger(brown.tagged_sents())
+        t1 = time()
+        print ("Time taken by NLTK for training: ",t1-t0)
+
+        sents_tokens = word_tokenize(f)
+        nltk_tags = []
+        t0 = time()
+        for sent in sents_tokens:
+            nltk_tags.append(tagger.tag(sent))
+        t1 = time()
+        print ("Time taken by NLTK to tag text: ",t1-t0)
+
+        pos_tagger_tags = []
+        for sent in sents_tokens:
+            pos_tagger_tags.append(self.viterbi(sent))
 
 
    
@@ -159,3 +185,4 @@ if __name__=="__main__":
     pos_tag.calc_Q()
     pos_tag.calc_R()
     pos_tag.test()
+    pos_tag.baseline_tagger()
