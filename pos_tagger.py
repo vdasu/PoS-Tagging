feaec8de7f26a47ebd439be109a76c8b5f69b2ce
Vishnu Dasu <vishnu98dasu@gmail.com>
2017-07-16 14:01:24 +0530

    Update Viterbi remove test

--- pos_tagger.py
+++ pos_tagger.py
@@ -29,7 +29,7 @@ class pos_tagger:
             sent.append('STOP') #Append STOP token to end of list
     
     def init_tags(self): #Function to initialise tags
-        sents = open("brown_trigram.txt").read().splitlines()
+        sents = open("brown_trigram.txt").read().splitlines()[:50000]
         for sent in sents:
             words = sent.split()
             for word in words:
@@ -38,7 +38,7 @@ class pos_tagger:
         self.tags_set = set(self.tags) #List of all unique tags in corpus
     
     def init_words_tags(self): #Function to initialise word-tag pairs
-        tagged_sents = open("brown_trigram.txt").read().splitlines()
+        tagged_sents = open("brown_trigram.txt").read().splitlines()[:50000]
         words_tags = []
         for tagged_sent in tagged_sents:
             tagged_words = tagged_sent.split()
@@ -71,24 +71,7 @@ class pos_tagger:
     def calc_R(self): #Function to calculate word-tag probabilities
         for word,tag in self.words_tags:
             self.R[(word,tag)]=float(self.words_tags_dict[(word,tag)])/float(self.unigrams_dict[(tag,)])
-
-    def test(self): #Test function
-
-        sent = "START START The grand jury commented. STOP"
-        tag = "START START AT JJ NN VBD  . STOP" 
-        prob_tag = 1.0
-        prob_words = 1.0
-        tot_prob = 1.0
-        sent_tokens = [j for i in word_tokenize(sent) for j in i]
-        tag_tokens = [j for i in word_tokenize(tag) for j in i]
-        for i in range(2,len(tag_tokens)-1):
-            prob_tag*=self.Q[(tag_tokens[i-2],tag_tokens[i-1],tag_tokens[i])]
-        for i,j in zip(sent_tokens,tag_tokens):
-            prob_words*=self.R[(i,j)]
-        tot_prob=prob_tag*prob_words
-        print (tot_prob)
         
-
     def viterbi(self,tokens): #Function returns list containing tokens and their respective POS tags
         
         n = len(tokens) #Length of input sentence
@@ -98,53 +81,55 @@ class pos_tagger:
 
         #Iterate through all tokens all store max probabilities and max arguments
         for k in range(1,n+1):
-            x = tokens[k-1]
-            #Sample space 'S' for each tag w,u,v  
-            W = self.tags_set
-            U = self.tags_set
-            V = self.tags_set
-            if k==1: #Both preceding tags for first word are START in a trigram model
-                W = ('START')
-                U = ('START')
-            if k==2: #One of the preceding tags for second word is START in a trigram model
-                W = ('START')
-            for u in U: #Tag at position k-1
-                for v in V: #Tag at position k
-                    max_prob = 0.0 #Stores maximum probability for each word
-                    max_arg = "" #Stores tag that maximises probability 
-                    for w in W: #Tag at position k-2
-                        temp_prob = psi[k-1][(w,u)]*self.Q[(w,u,v)]*self.R[(x,v)] #Calculate probability for tag sequence at position k
-                        if temp_prob>max_prob:
-                            if temp_prob!=0.0:
-                                max_prob = temp_prob
-                                max_arg = w 
-                    psi[k][(u,v)] = max_prob #Store maximum probability for positon k and tags u,v
-                    back_ptr[k][(u,v)] = max_arg #Store tag that maximises psi (backpointer)
+        	x = tokens[k-1]
+        	#Sample space 'S' for each tag w,u,v  
+        	W = self.tags_set
+        	U = self.tags_set
+        	V = self.tags_set
+        	
+        	if k==1: #Both preceding tags for first word are START in a trigram model
+        		W = ('START',)
+        		U = ('START',)
+        	if k==2: #One of the preceding tags for second word is START in a trigram model
+        		W = ('START',)
+        	for u in U: #Tag at position k-1
+        		for v in V: #Tag at position k
+        			max_prob = 0.0 #Stores maximum probability for each word
+        			max_arg = "" #Stores tag that maximises probability 
+        			for w in W: #Tag at position k-2
+        				if (self.R[(x,v)]) == 0.0:
+        					continue
+        				if psi[k-1][(w,u)]!=0.0:
+        					temp_prob = psi[k-1][(w,u)]*self.Q[(w,u,v)]*self.R[(x,v)] #Calculate probability for tag sequence at position k
+        				if temp_prob>max_prob:
+        					max_prob = temp_prob
+        					max_arg = w     
+        			if max_prob!=0.0:
+        				psi[k][(u,v)] = max_prob #Store maximum probability for positon k and tags u,v
+        				back_ptr[k][(u,v)] = max_arg #Store tag that maximises psi (backpointer)
+        
+        #Back Track to determine tag sequence
+        max_prob = 0.0 
+        max_u = "" 
+        max_v = ""
+        for u in self.tags_set: #Tag at position n-1
+        	for v in self.tags_set: #Tag at position n
+        		if self.Q[(u,v,'STOP')]*psi[n][(u,v)]!=0.0:
+        			temp_prob = psi[n][(u,v)]*self.Q[(u,v,'STOP')]
+        			if temp_prob>max_prob:
+        				max_prob = temp_prob
+        				max_u = u
+        				max_v = v
 
-            #Back Track to determine tag sequence
-            max_prob = 0.0 
-            max_u = "" 
-            max_v = ""
-            for u in self.tags_set: #Tag at position n-1
-                for v in self.tags_set: #Tag at position n
-                    temp_prob = psi[n][(u,v)]*self.Q[(u,v,'STOP')]
-                    if temp_prob>max_prob:
-                        if temp_prob!=0.0:
-                            max_prob = temp_prob
-                            max_u = u
-                            max_v = v
-
-            t = [None]*(n+1) #Initialise tag sequence
-            t[n] = max_v
-            t[n-1] = max_u
-            for k in range(n-2,0,-1):
-                t[k] = back_ptr[k+2][(t[k+1],t[k+2])] #Back track to complete tag sequence
-            
-            tagged_sent = [] #List to store token-tag tuple 
-            for token,tag in zip(tokens,t[1:]):
-                tagged_sent.append((token,tag))
-            return tagged_sent
-
+        t = [None]*(n+1) #Initialise tag sequence
+        t[n] = max_v
+        t[n-1] = max_u
+        for k in range(n-2,0,-1):
+        	t[k] = back_ptr[k+2][(t[k+1],t[k+2])] #Back track to complete tag sequence
+        tagged_sent = [] #List to store token-tag tuple 
+        for token,tag in zip(tokens,t[1:]):
+        	tagged_sent.append((token,tag))
+        return tagged_sent
 
     def baseline_tagger(self):
 
@@ -228,6 +213,8 @@ class pos_tagger:
         print ("Number of unique tags generated by NLTK: ",len([i for i in nltk_tag_count.keys()]))
 
         print ("Number of unique tags generated by pos_tagger: ",len([i for i in pos_tag_count.keys()]))
+
+        print (pos_tagger_tags)
 
    
 if __name__=="__main__":
